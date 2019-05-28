102ee3b9a4abdf6d5424235cf740df01be5de74f
Jeremy Solbrig <jeremy.solbrig@colostate.edu>
2019-05-28 14:40:08 -0600

    Updating project URL and giving credit to audreyr

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 import os
+from glob import glob
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+def remove_directory(dirpath):
+    for fname in glob('{}/*'.format(os.path.join(PROJECT_DIRECTORY, dirpath))):
+        remove_file(fname)
+    os.rmdir(os.path.join(PROJECT_DIRECTORY, dirpath))
 
 
 if __name__ == '__main__':
@@ -16,6 +23,9 @@ if __name__ == '__main__':
 
     if '{{ cookiecutter.use_pytest }}' == 'y':
         remove_file('tests/__init__.py')
+
+    # if '{{ cookiecutter.make_satpy_composite }}' != 'y':
+    #     remove_directory('{{cookiecutter.project_slug}}/composites')
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
--- {{cookiecutter.project_slug}}/README.rst
+++ {{cookiecutter.project_slug}}/README.rst
@@ -37,7 +37,8 @@ Features
 Credits
 -------
 
-This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+This package was created with Cookiecutter_ and the `jsolbrig/cookiecutter-satpy`_ project template which is heavily based on the `audreyr/cookiecutter-pypackage`_ template.
 
 .. _Cookiecutter: https://github.com/audreyr/cookiecutter
 .. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+.. _`jsolbrig/cookiecutter-satpy`: https://github.com/jsolbrig/cookiecutter-satpy
